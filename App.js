0b4e1ee7a96bead0fb7ee7ff0508bce8dc7bb9e4
18016222 <amalyqayum@gmail.com>
2019-11-11 15:29:12 +0800

    about rp testing

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
+  console.log('I am a console statement');
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -40,29 +41,25 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text>Welcome to C308 Web Frameworks!</Text>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+              <Text style={styles.highlight}>Welcome to Republic Polytechnic</Text>
+              <Text style={styles.sectionTitle}>About Us</Text>
+              <Text style={styles.sectionDescription}>At <Text style={styles.highlight}>Republic Polytechnic</Text>, we will help you to Discover your potential, Transform opportunities into realities and Achieve your goals. We will support you on your unique learning journey, every step of the way.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
+              <Text style={styles.sectionTitle}>Engaging Learning Experience</Text>
+              <Text style={styles.sectionDescription}>Fun, active, engaging and goes beyond listening to lectures and memorising facts. Through our <Text style={styles.highlight}>Problem-based Learning (PBL)</Text> and other learner-centred approaches, you will have the opportunity to think critically, analyse problems and develop solutions while working in groups.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
+              <Text style={styles.sectionTitle}>Further Studies</Text>
+              <Text style={styles.sectionDescription}>As an RP graduate, you will have skills that will resonate throughout your entire life. Your professionalism, problem-solving skills and passion for life will give you an edge in any career you choose to pursue. Many RP graduates have gained recognition in various industries, both locally and overseas.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionTitle}>See more at</Text>
               <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
+                https://www.rp.edu.sg/home
               </Text>
             </View>
             <LearnMoreLinks />
